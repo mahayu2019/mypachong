cdcfdc2eac5e69d96f2fae4e1925fcb2b3ed32e8
Administrator <2353987275@qq.com>
2021-11-26 13:22:06 +0800

    基本

--- pc120/eg006/getCosers.py
+++ pc120/eg006/getCosers.py
@@ -10,7 +10,8 @@ import os
 import requests
 
 headers = {
-    "user-agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36",
+    "user-agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/90.0.4430.85 Safari/537.36",
     "host": 'www.cosplay8.com'
 }
 
